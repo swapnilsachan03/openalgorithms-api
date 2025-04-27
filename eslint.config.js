5fa1a9e1f9c7d0abb3c5df4bac4e80b972e919b9
Swapnil Sachan <swapnil.sachan2003@gmail.com>
2025-04-27 16:27:42 +0530

    feat: add pre-commit checks using husky

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.ts", "**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2020,
+    },
+    rules: {
+      "no-console": ["error", { allow: ["warn", "error"] }],
+    },
+  }
+);
